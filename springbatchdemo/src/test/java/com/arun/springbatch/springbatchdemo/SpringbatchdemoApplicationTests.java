224bd50de4bf5051a75b08555134b52b3d354f1c
Arun <arun@Aruns-MacBook-Pro.local>
2020-09-07 17:54:12 -0700

    commiting springbatch demo

---
+++ springbatchdemo/src/test/java/com/arun/springbatch/springbatchdemo/SpringbatchdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.springbatch.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
