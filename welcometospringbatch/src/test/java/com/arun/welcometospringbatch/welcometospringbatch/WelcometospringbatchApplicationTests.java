8f29a2e150ce84edc41cce536bee5ca0ec5c5376
Arun <arun@Aruns-MacBook-Pro.local>
2020-09-07 18:51:50 -0700

    examplewithtwosteps

---
+++ welcometospringbatch/src/test/java/com/arun/welcometospringbatch/welcometospringbatch/WelcometospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.welcometospringbatch.welcometospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcometospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
